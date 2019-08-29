942a9d8495c9a158c5eea4f62595e98a8ac0baf9
Margarita <margaritk@inbox.ru>
2019-08-29 18:18:55 +0300

    Убирает магические числа в константы

--- src/components/trip-day-item.js
+++ src/components/trip-day-item.js
@@ -1,6 +1,17 @@
 import constant from '../constant.js';
 import AbstractComponent from './abstract-component.js';
 
+const LetterNumber = {
+  FIRST_OF_YEAR: 8,
+  LAST_OF_YEAR: 10,
+  FIRST_OF_MONTH: 4,
+  LAST_OF_MONTH: 7,
+  FIRST_OF_DAY: 11,
+  LAST_OF_DAY: 16,
+  FIRST_OF_MONTH_DAY: 4,
+  LAST_OF_MONTH_DAY: 11,
+};
+
 export default class TripDayItem extends AbstractComponent {
   constructor(day, index) {
     super();
@@ -13,8 +24,8 @@ export default class TripDayItem extends AbstractComponent {
       <div class="day__info">
         <span class="day__counter">${this._index === 0 || this._index ? (this._index + 1) : ``}</span>
         <time class="day__date" datetime="
-        ${this._day ? `${this._day.slice(11, 16)}-${constant.monthMap[this._day.slice(4, 7)]}-${this._day.slice(8, 10)}` : ``}">
-          ${this._day ? `${this._day.slice(4, 11)}` : ``}
+        ${this._day ? `${this._day.slice(LetterNumber.FIRST_OF_DAY, LetterNumber.LAST_OF_DAY)}-${constant.monthMap[this._day.slice(LetterNumber.FIRST_OF_MONTH, LetterNumber.LAST_OF_MONTH)]}-${this._day.slice(LetterNumber.FIRST_OF_YEAR, LetterNumber.LAST_OF_YEAR)}` : ``}">
+          ${this._day ? `${this._day.slice(LetterNumber.FIRST_OF_MONTH_DAY, LetterNumber.LAST_OF_MONTH_DAY)}` : ``}
         </time>
       </div>
 
